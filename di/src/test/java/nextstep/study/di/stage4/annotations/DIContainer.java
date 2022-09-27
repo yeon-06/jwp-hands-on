be5b6b4b30fb6827a9f69d5d6a1698a278260d52
yeonLog <53105735+yeon-06@users.noreply.github.com>
2022-09-27 16:45:04 +0900

    [220927] DI 활용하기 (#5)
    * style: 생성자 위치 변경
    
    * feat: DI 컨테이너 구현하기
    
    - 현재: 모든 필드 목록을 가져와 null 값인 필드만 값 주입
    - 개선 사항: 빈으로 등록되지 않은 객체를 주입시키려고 하는 경우가 존재
    
    * feat: @Repository, @Service 어노테이션을 단 클래스들 모두 스캔
    
    * feat: DIContainer에 bean 등록
    
    * feat: 원하는 bean 찾기
    
    * feat: @Inject 어노테이션이 달린 필드 주입

--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -9,16 +14,67 @@ class DIContainer {
 
     private final Set<Object> beans;
 
-    public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+    private DIContainer(final Set<Class<?>> classes) {
+        this.beans = createBeans(classes);
+        initialBeans();
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::createInstance)
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private Object createInstance(final Class<?> clazz) {
+        try {
+            Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        return new DIContainer(ClassPathScanner.getAllClassesInPackage(rootPackageName));
+    }
+
+    private void initialBeans() {
+        for (Object bean : beans) {
+            Field[] fields = bean.getClass().getDeclaredFields();
+            initialFields(bean, fields);
+        }
+    }
+
+    private void initialFields(final Object bean, final Field[] fields) {
+        for (Field field : fields) {
+            setField(bean, field);
+        }
+    }
+
+    private void setField(final Object bean, final Field field) {
+        try {
+            if (hasInjectAnnotation(field)) {
+                field.setAccessible(true);
+                field.set(bean, getBean(field.getType()));
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean hasInjectAnnotation(final Field field) {
+        return field.isAnnotationPresent(Inject.class);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        Object instance = beans.stream()
+                .filter(aClass::isInstance)
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("해당 클래스 타입의 빈은 존재하지 않습니다. -> " + aClass));
+
+        return (T) instance;
     }
 }
